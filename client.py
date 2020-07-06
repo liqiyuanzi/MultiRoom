a47279ea52fe49aa75aeb6d328bea66b44494849
wang <454940304@qq.com>
2020-07-06 21:27:38 +0800

    first commit

---
+++ client.py
@@ -0,0 +1,23 @@
+import websocket
+
+def on_message(ws, message):
+    print(message)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws):
+    print("### closed ###")
+
+def on_open(ws):
+    pass
+
+
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:8888/ws/",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
